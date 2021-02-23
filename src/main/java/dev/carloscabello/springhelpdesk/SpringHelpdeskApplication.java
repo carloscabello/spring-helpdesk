57e01b19ea6bf693c4b495fc03ef7d472701623a
carloscabello <git@carloscabello.net>
2021-02-23 04:40:19 +0100

    Base project from Spring Initializr

---
+++ src/main/java/dev/carloscabello/springhelpdesk/SpringHelpdeskApplication.java
@@ -0,0 +1,13 @@
+package dev.carloscabello.springhelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/carloscabello/springhelpdesk/SpringHelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.carloscabello.springhelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
